29ba04a9f97e8751c93280feb552bfd4694cd24e
Andrew Chavez <adchavez@gmail.com>
2015-01-08 17:02:31 -0600

    Use images for open splash

--- src/js/index.js
+++ src/js/index.js
@@ -10,52 +10,20 @@ $(function() {
     $('#fatal-consequences').addClass('animated fadeInUp');
   }, 750);
 
-  // Slowly bring in children
-  /*
-  $('.children-step-1').addClass('animated fadeIn');
+  // Fade in images
   window.setTimeout(function() {
-    $('.children-step-2').addClass('animated fadeIn');
-  }, 400);
-  window.setTimeout(function() {
-    $('.children-step-3').addClass('animated fadeIn');
-  }, 800);
-  window.setTimeout(function() {
-    $('.children-step-4').addClass('animated fadeIn');
-  }, 1200);
-  */
+    $('#image-1').addClass('animated fadeInRight');
+    $('#image-2').addClass('animated fadeIn');
+    $('#image-3').addClass('animated fadeInLeft');
+  }, 1150);
 
   // Fade everything away on scroll
   $(window).one('scroll', function() {
+    if($(window).width() > 480) {
+      $('.image-pack').addClass('animated fadeOut');
+    }
     $('#overview-text').addClass('animated fadeIn');
-    $('#child-wrapper').addClass('animated fadeOut');
     $('#scroll-prompt').addClass('animated fadeOutDown');
   });
-
-  var childrenEl = $('#child-wrapper');
-
-  function rand() {
-    return (Math.random() * 100).toString() + '%';
-  }
-
-  function placeChild() {
-    $('<i class="fa fa-child"></i>')
-      .css('left', rand())
-      .css('top', rand())
-      .addClass(function() {
-        return 'children-depth-' + _.random(1, 5).toString();
-      })
-      .addClass(function() {
-        return 'children-step-' + _.random(1, 4).toString();
-      })
-      .addClass(function() {
-        if(Math.random() >= 0.5) {
-          return 'girl';
-        }
-        return 'boy';
-      })
-      .appendTo(childrenEl);
-  }
-
-  //_.times(400, placeChild);
 
 });
