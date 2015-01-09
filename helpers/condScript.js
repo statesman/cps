946883f00b085873ac5d4b3e3941a802644268ca
Andrew Chavez <adchavez@gmail.com>
2015-01-08 18:47:20 -0600

    Only insert a story-specific js file it there is one

---
+++ helpers/condScript.js
@@ -0,0 +1,17 @@
+var Handlebars = require('handlebars'),
+    fs = require('fs');
+
+/*
+* Insert a link to a script, but only if it exists
+*
+* @filename: the filename in the public/dist/ folder
+*/
+module.exports = function(filename) {
+  var scriptPath = __dirname + '/../public/dist/' + filename + '.js';
+
+  var exists = fs.existsSync(scriptPath);
+
+  if(exists) {
+    return new Handlebars.SafeString('<script src="dist/' + filename + '.js"></script>');
+  }
+};
