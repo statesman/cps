28f1cd31fcbcabc63283cd9d7ffd6b74afb28d87
Andrew Chavez <adchavez@gmail.com>
2015-01-09 20:19:30 -0600

    Rip out overview scroll-to-reveal animations

--- src/js/index.js
+++ src/js/index.js
@@ -8,22 +8,9 @@ $(function() {
   $('#missed-signs').addClass('animated fadeInDown');
   window.setTimeout(function() {
     $('#fatal-consequences').addClass('animated fadeInUp');
-  }, 750);
-
-  // Fade in images
+  }, 650);
   window.setTimeout(function() {
-    $('#image-1').addClass('animated fadeInRight');
-    $('#image-2').addClass('animated fadeIn');
-    $('#image-3').addClass('animated fadeInLeft');
-  }, 1150);
-
-  // Fade everything away on scroll
-  $(window).one('scroll', function() {
-    if($(window).width() > 480) {
-      $('.image-pack').addClass('animated fadeOut');
-    }
-    $('#overview-text').addClass('animated fadeIn');
-    $('#scroll-prompt').addClass('animated fadeOutDown');
-  });
+    $('#how').addClass('animated fadeIn');
+  }, 1300);
 
 });
